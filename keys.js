b87830f0848e0238c3f5480fe291ffb3375b530b
Lindsay Preston <lindsayarthur303@gmail.com>
2019-08-24 13:15:16 -0600

    fixed

--- keys.js
+++ keys.js
@@ -1,16 +1,6 @@
+console.log('this is loaded');
+
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
-
-exports.bandsintown = {
-    id: process.env.BANDSINTOWN_ID
-}
-
-exports.omdb = {
-    id: process.env.OMDB_ID
-}
-
-exports.mapquest = {
-    id: process.env.MAPQUEST_ID
-}
--- liri.js
+++ liri.js
@@ -1,142 +1,184 @@
-// REQUIRE .env FILE
+//Required 
 require("dotenv").config();
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 
-// REQUIRE REQUEST
-let request = require("request");
+//For Spotify keys
+const env = process.env;
 
-// REQUIRE MOMENT
-const moment = require('moment');
+var Spotify = require('node-spotify-api');
 
-//REQUIRE FILE SYSTEMS
-const fs = require("fs");
-
-// LINK KEY PAGE
-const keys = require("./keys.js");
-
-// INITIALIZE SPOTIFY
-const Spotify = require("node-spotify-api");
-const spotify = new Spotify(keys.spotify);
-
-// OMDB AND BANDS IN TOWN API'S
-let omdb = (keys.omdb);
-let bandsintown = (keys.bandsintown);
+var spotify = new Spotify({
+    id: env.SPOTIFY_ID,
+    secret: env.SPOTIFY_SECRET
+});
 
 
-// TAKE USER COMMAND AND INPUT
-let userInput = process.argv[2];
-let userQuery = process.argv.slice(3).join(" ");
+//Input on CLI
+var query = process.argv;
+var type = process.argv[2];
+var array = [];
+
+//Loop through and join name of arguments after file name
+for (var i = 3; i < query.length; i++) {
+    array.push(query[i]);
+    array.push("+")
+}
+
+array.splice(-1); //Get rid of last plus sign, if left errors caused
+var finalSearch = array.join(""); //Search query joined together to form string for any query below
+
+//Switch statement to determine type selected. (Ex. concert-this, movie-this, etc.)
+switch (type) {
+    case 'concert-this':
+        concertMe()
+        break;
+    case 'spotify-this-song':
+        spotifyIt()
+        break;
+    case 'movie-this':
+        movieThis()
+        break;
+    case 'do-what-it-says':
+        itSays()
+        break;
+    default:
+        console.log("No type value found");
+}
 
 
-// APP LOGIC
-function userCommand(userInput, userQuery) {
-    // make a decision based on the command
-    switch (userInput) {
-        case "concert-this":
-            concertThis();
-            break;
-        case "spotify-this":
-            spotifyThisSong();
-            break;
-        case "movie-this":
-            movieThis();
-            break;
-        case "do-this":
-            doThis(userQuery);
-            break;
-        default:
-            console.log("I don't understand");
-            break;
+// node liri.js concert-this
+
+function concertMe() {
+    if (finalSearch === "") {
+        console.log('\n')
+        console.log("No Artist entered. Please enter an Artist")
+        console.log('\n')
+    } else {
+        axios.get("https://rest.bandsintown.com/artists/" + finalSearch + "/events?app_id=codingbootcamp").then(
+        function (response) {
+           if(response.data.length <= 0) {
+               console.log("No info for this Artist")
+           }else {
+            for(var i=0; i < response.data.length; i++) {
+
+                var currData = `\n
+    Venue: ${response.data[i].venue.name}
+    Location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}
+    Event Date: ${moment(response.data[i].datetime).format('LL')}
+            `
+            console.log(currData)
+            }
+           }
+           
+            dataLog(currData)
+        }
+    );
     }
 }
 
-userCommand(userInput, userQuery);
+// node liri.js spotify-this-song
 
-function concertThis() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
-    // USE REQUEST AS OUR QUERY URL USING OUR USER QUERY VARIABLE AS THE PARAMETERS OF OUR SEARCH
-    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
-        // IF THERE IS NO ERROR GIVE US A 200 STATUS CODE (EVERYTHING OK!)
-        if (!error && response.statusCode === 200) {
-            // CAPTURE DATA AND USE JSON TO FORMAT
-            let userBand = JSON.parse(body);
-            // PARSE DATA AND USE FOR LOOP TO ACCESS PATHS TO DATA
-            if (userBand.length > 0) {
-                for (i = 0; i < 1; i++) {
+function spotifyIt() {
 
-                    // CONSOLE DESIRED DATA USING E6 SYNTAX
-                    console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+    if (finalSearch === "") {
+        finalSearch = "ace+of+base+the+sign"
+    }
 
-                    // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
-                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
-                    console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
-                };
-            } else {
-                console.log('Band or concert not found!');
-            };
-        };
-    });
-};
-
-function spotifyThisSong() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
-
-    // IF USER QUERY NOT FOUND, PASS VALUE OF "ACE OF BASE" 
-    if (!userQuery) {
-        userQuery = "the sign ace of base"
-    };
-
-    // SPOTIFY SEARCH QUERY FORMAT
     spotify.search({
-        type: 'track',
-        query: userQuery,
-        limit: 1
-    }, function (error, data) {
-        if (error) {
-            return console.log('Error occurred: ' + error);
+        type: 'artist,track',
+        query: finalSearch
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
         }
-        // COLLECT SELECTED DATA IN AN ARRAY
-        let spotifyArr = data.tracks.items;
+        console.log('\n')
 
-        for (i = 0; i < spotifyArr.length; i++) {
-            console.log(`\nBA DA BOP!  That's for you...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
-        };
+        var currData = `\n
+    Artist: ${data.tracks.items[0].artists[0].name}
+    Track: ${data.tracks.items[0].name}
+    Preview: ${data.tracks.items[0].preview_url}
+    Album: ${data.tracks.items[0].album.name}
+            `
+            console.log(currData)
+            dataLog(currData)
+
     });
 }
 
+// node liri.js movie-this
+
 function movieThis() {
-    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
-    if (!userQuery) {
-        userQuery = "mr nobody";
-    };
-    // REQUEST USING OMDB API
-    request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=86fe999c", function (error, response, body) {
-        let userMovie = JSON.parse(body);
 
-        // BECAUSE THE ROTTEN TOMATOES RATING WAS NESTED IT WAS NECESSARY TO CAPTURE ITS VALUES IN AN ARRAY TO CREATE A PATH
-        let ratingsArr = userMovie.Ratings;
-        if (ratingsArr.length > 2) {}
+    if (finalSearch === "") {
+        finalSearch = "mr+nobody"
+    }
 
-        if (!error && response.statusCode === 200) {
-            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
-        } else {
-            return console.log("Movie able to be found. Error:" + error)
-        };
-    })
-};
-
-function doThis() {
-    // UTILIZE THE BUILT IN READFILE METHOD TO ACCESS RANDOM.TXT
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
+    axios.get("http://www.omdbapi.com/?t=" + finalSearch + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+        
+            var currData = `\n
+    Title: ${response.data.Title}
+    Released: ${response.data.Year}
+    IMDB Rating: ${response.data.imdbRating}
+    Rotten Tomatos Rating: ${response.data.Ratings[1].Value}
+    Country: ${response.data.Country}
+    Language: ${response.data.Language}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}
+            `
+            console.log(currData)
+            dataLog(currData)
         }
-        // CATCH DATA AND USE THE .SPLIT() METHOD TO SEPARATE OBJECTS WITHIN OUR NEW ARRAY
-        let dataArr = data.split(",");
+    );
 
-        // TAKE OBJECTS FROM RANDOM.TXT TO PASS AS PARAMETERS
-        userInput = dataArr[0];
-        userQuery = dataArr[1];
-        // CALL OUR FUNCTION WITH OUR NEW PARAMETERS...
-        userCommand(userInput, userQuery);
-    });
-};
+    
+}
+
+// node liri.js do-what-it-says
+
+function itSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+      
+        finalSearch = dataArr[1];
+        spotifyIt()
+      });
+}
+
+//Input Logger - see log.txt
+
+var logQuery = query.splice(0,2)
+logQuery =  "\n" + query.join(" ") + "\n"
+console.log(logQuery)
+
+fs.appendFile("log.txt", logQuery, function(err) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Log Updated");
+    }
+  
+  });
+
+//Data Logger - see log.txt
+
+function dataLog(data) {
+    fs.appendFile("log.txt", data, function(err) {
+
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Log Updated");
+        }
+      
+      });
+  }
+
